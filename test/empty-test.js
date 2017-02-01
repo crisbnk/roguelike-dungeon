e1cb1ea58bbe5180d200610e76bd24753a9731c2
Cristiano Bianchi <cristiano.bianchi@gmail.com>
2017-02-01 23:58:58 +0100

    Starting with Mocha and Chai testing

---
+++ test/empty-test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+const foo = 'bar';
+const beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+
+expect(foo).to.be.a('string');
+expect(foo).to.equal('bar');
+expect(foo).to.have.lengthOf(3);
+expect(beverages).to.have.property('tea').with.lengthOf(3);
