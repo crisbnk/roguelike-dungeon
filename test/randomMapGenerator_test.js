4ef81424af2212694905b7117000e40670189a8f
Cristiano Bianchi <cristiano.bianchi@gmail.com>
2017-02-10 00:03:41 +0100

    Add tests in randomMapGenerator_test.js

--- test/randomMapGenerator_test.js
+++ test/randomMapGenerator_test.js
@@ -2,7 +2,21 @@ import { randomMapGenerator } from '../src/js/randomMapGenerator';
 import { expect } from 'chai';
 
 describe('randomMapGenerator', () => {
+  const height = 30;
+  const lenght = 50;
+  const attempts = 150;
+  const randomMap = randomMapGenerator(height, lenght, attempts);
+
   it('should be a function', () => {
     expect(randomMapGenerator).to.be.a('function');
+  });
+
+  it('should be an array', () => {
+    expect(randomMap).to.be.an('array');
+  });
+
+  it('should have right lenght', () => {
+    expect(randomMap).to.have.lengthOf(height);
+    expect(randomMap[0]).to.have.lengthOf(lenght);
   });
 });
