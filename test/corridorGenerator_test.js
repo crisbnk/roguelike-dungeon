6c2197a623c9c2b66223a44ded717114219c1b06
Cristiano Bianchi <cristiano.bianchi@gmail.com>
2017-02-12 22:47:35 +0100

    Added tests to corridorGenerator_test.js

--- test/corridorGenerator_test.js
+++ test/corridorGenerator_test.js
@@ -1,9 +1,23 @@
 import { corridorGenerator } from '../src/js/corridorGenerator';
+import { randomMapGenerator } from '../src/js/randomMapGenerator.js';
 import { expect } from 'chai';
 
 describe('corridorGenerator', () => {
+  const height = 30;
+  const lenght = 50;
+  const attempts = 150;
+  const randomMap = randomMapGenerator(height, lenght, attempts);
+  const corridorArray = corridorGenerator(randomMap);
 
   it('should be a function', () => {
     expect(corridorGenerator).to.be.a('function');
+  });
+
+  it('should be an array', () => {
+    expect(corridorArray).to.be.an('array');
+  });
+
+  it('should be an object', () => {
+    expect(corridorArray[0]).to.be.an('object');
   });
 });
